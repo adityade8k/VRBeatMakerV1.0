f1fc7b4ded33efa139db50697c18fb956e4b0041
Aditya <229662497+adityade8k@users.noreply.github.com>
2025-10-13 16:55:54 -0400

    basic setup and interactions done

--- src/components/GLTFModels.jsx
+++ src/components/GLTFModels.jsx
@@ -22,12 +22,12 @@ function Model({ url, index, total }) {
   const gltf = useGLTF(url)
   const radius = 1.2
   const angle = total > 1 ? THREE.MathUtils.mapLinear(index, 0, total - 1, -0.6, 0.6) : 0
-  const x = Math.sin(angle) * radius
-  const z = -1.5 + Math.cos(angle) * 0.2
+  const x = index*0.8
+  const z = 0
   const y = 0.5
 
   return (
-    <group position={[x, y, z]} rotation={[0, angle * 0.3, 0]}>
+    <group position={[x-1, y, z]} rotation={[0, 0, 0]} scale = {[0.25, 0.25, 0.25]}>
       <primitive object={gltf.scene} />
     </group>
   )
